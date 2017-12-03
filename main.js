d51592147e4a4b3fd8cce641f69facf005adf9f9
shota <2525.natuta@gmail.com>
2017-12-04 00:24:32 +0900

    HTML

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ app.on('window-all-closed', function() {
 // Electronの初期化完了後に実行
 app.on('ready', function() {
   // メイン画面の表示。ウィンドウの幅、高さを指定できる
-  mainWindow = new BrowserWindow({width: 400, height: 600});
+  mainWindow = new BrowserWindow({width: 600, height: 850});
   mainWindow.loadURL('file://' + __dirname + '/index.html');
 
   // ウィンドウが閉じられたらアプリも終了
