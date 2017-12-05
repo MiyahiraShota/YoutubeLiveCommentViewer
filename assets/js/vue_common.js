41edf6e70fe4cef510a0973b582ff4d27c6d84f3
miyahira <miyahira@aidev.net>
2017-12-05 15:45:06 +0900

    jQuery/axiosの読み込み　detaの追加

--- assets/js/vue_common.js
+++ assets/js/vue_common.js
@@ -1,9 +1,29 @@
+const $ = require('jquery');
+const axios = require('axios');
+
 new Vue({
     el: '.main_wrap',
     data: {
-        hello: ''
+        hello: '',
+        api_key: '',
+        video_id: 'mpS9LqdnBRI'
     },
     created () {
         this.hello = 'okokokok'
+    },
+    methods: {
+        connection: function () {
+            let url = "https://www.googleapis.com/youtube/v3/videos?part=liveStreamingDetails&id=" + this.video_id + "&key=" + this.api_key;
+            console.log(url);
+            axios.get(url, {
+                timeout: 30000
+            })
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+        }
     }
 })
