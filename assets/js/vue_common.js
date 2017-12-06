bd7504a699d8738cc273914c6dc52760d4c62f55
miyahira <miyahira@aidev.net>
2017-12-06 15:11:39 +0900

    コメントを取得する部分を追加

--- assets/js/vue_common.js
+++ assets/js/vue_common.js
@@ -4,22 +4,46 @@ const axios = require('axios');
 new Vue({
     el: '.main_wrap',
     data: {
-        hello: '',
         api_key: '',
-        video_id: 'mpS9LqdnBRI'
+        video_id: '',
+        concurrentViewers: 0,
+        activeLiveChatId: null,
+        liveChatData: []
     },
     created () {
         this.hello = 'okokokok'
     },
     methods: {
-        connection: function () {
+        cconnection: function () {
             let url = "https://www.googleapis.com/youtube/v3/videos?part=liveStreamingDetails&id=" + this.video_id + "&key=" + this.api_key;
-            console.log(url);
+            let $this = this;
             axios.get(url, {
                 timeout: 30000
             })
             .then(function (response) {
-                console.log(response);
+                if (response.data.items[0]) {
+                    $this.concurrentViewers = response.data.items[0].liveStreamingDetails.concurrentViewers;
+                    $this.activeLiveChatId = response.data.items[0].liveStreamingDetails.activeLiveChatId;
+
+                    $this.chatLoad();
+                }
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+        },
+        chatLoad: function () {
+            let url = "https://www.googleapis.com/youtube/v3/liveChat/messages?part=snippet,authorDetails&liveChatId=" + this.activeLiveChatId + "&key=" + this.api_key;
+            console.log(url);
+
+            let $this = this;
+            axios.get(url, {
+                timeout: 30000
+            })
+            .then(function (response) {
+                if (response.data.items) {
+                    $this.liveChatData = response.data.items;
+                }
             })
             .catch(function (error) {
                 console.log(error);
